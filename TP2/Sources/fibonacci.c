1e9ca794d31bc212b6b7c4c7f2c2c971f9035c92
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-03 17:37:48 +0200

    Create fibonacci.c

---
+++ TP2/Sources/fibonacci.c
@@ -0,0 +1,28 @@
+/* Bibliothèques */
+
+#include <stdio.h>
+
+/*---------------------------------------------------------*/
+
+// Déclaration d'une constante 
+#define n 11
+
+int main() {
+	
+	int U0 = 1;
+	int U1 = 1;
+	int tab[n] = {U0,U1};
+	for (int i = 2 ; i <= n ; i++) {
+	
+		tab[i] = tab[i-1] + tab[i-2];
+	}
+
+	for ( int j = 0 ; j < sizeof(tab)/sizeof(int); j++){
+	
+	printf("U%d = %d  ",j,tab[j]);
+
+	}
+
+
+	return 0 ;
+}
