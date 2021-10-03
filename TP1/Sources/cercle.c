a0643766ce8250d47d60ddcb79825bc7f702f863
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-03 12:14:58 +0200

    Update cercle.c

--- TP1/Sources/cercle.c
+++ TP1/Sources/cercle.c
@@ -1,11 +1,16 @@
+/* Bibliothèques*/
+
 #include <stdio.h>
 #include <math.h>
+
+/* -----------------------------------------------------------------------------------*/
 int main() {
-	float rayon = 5.5 ;
+	printf("\n------ Exercice 1.2 : Cercle ------\n\n");
+	float rayon = 5 ;
 	//aire = M_pi*powf(rayon,2);
 	//perim = 2*M_PI*rayon;
-	printf("l'aire du cercle de rayon %.2f est : %.4f\n",rayon,M_PI*rayon*rayon);
-	printf("le périmètre du cercle de raon %.2f est : %.4f\n",rayon,2*M_PI*rayon);
+	printf("l'aire d'un cercle de rayon %.2f est : %.4f\n",rayon,M_PI*rayon*rayon); 
+	printf("le périmètre d'un cercle de rayon %.2f est : %.4f\n\n",rayon,2*M_PI*rayon);
 	return 0;
 }
 
