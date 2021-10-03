461fa07bd608b858198f9dcf10c591fa48d84783
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-03 12:46:21 +0200

    Update boucles.c

--- TP1/Sources/boucles.c
+++ TP1/Sources/boucles.c
@@ -1,8 +1,19 @@
+/* Bibliothèques */
+
 #include <stdio.h>
+
+/*-----------------------------------------------------------------------------------*/
+
 int main () {
-	for (int i = 0; i < 5; i++){
+	printf("\n------ Exercice 1.6 : boucles ------\n\n");
+	int compteur = 5 ;
+	
+	/*---------------------------------------------*/
+	
+	printf("* Méthode N°1 : Boucle For *\n\n");
+	for (int i = 0; i < compteur; i++){
 		for( int j =0; j <= i; j++){
-			if ( j >=1 & j < i & i<4 ) {
+			if ( j >=1 & j < i & i<compteur-1 ) {
 				printf("# ");
 			}
 			else {
@@ -12,5 +23,62 @@ int main () {
 		}	
 	printf("\n");
 	}
+	
+	/*---------------------------------------------*/
+	
+	printf("\n");
+	printf("* Méthode N°2 : Boucle For *\n\n");
+		
+	for (int i = 0 ; i <= compteur ; i++) {
+		if (i < compteur) {
+			for (int j = 0 ; j <= i ; j++) {
+				if (j!=i & j!=0) {
+					printf("# ");
+				} else {
+					printf("* ");
+				}
+			}
+		} else {
+			for (int j = 0 ; j <= i ; j++) {
+				printf("* ");
+			}
+		}
+		printf("\n");
+	}
+	
+	/*---------------------------------------------*/
+	
+	printf("\n");
+	printf("* Méthode N°3 : Boucle While *\n");
+	int i = 0;
+	printf("\n");
+
+	while (i <= compteur) {
+
+		int j = 0;
+		if (i < compteur) {
+			j=0;
+			while (j <= i) {
+				if (j!=i & j!=0) {
+					printf("# ");
+				} else {
+					printf("* ");
+				}
+				j++;
+			}
+
+		} else {
+			j=0;
+			while (j <= i) {
+				printf("* ");
+				j++;
+			}
+
+		}
+		printf("\n");
+		i++;
+
+	}
+	printf("\n");
 	return 0;
 }
