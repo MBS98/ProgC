1be10284f3d2e235efca6e22fabf64e9957d045b
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-03 13:07:13 +0200

    Create binaire.c

---
+++ TP1/Sources/binaire.c
@@ -0,0 +1,50 @@
+/* Bibliothèques */
+
+#include <stdio.h>
+#include <math.h>
+#include<float.h>
+
+/*--------------------------------------------------------------------------------------*/
+
+int main () {
+	int N = 4069 ;
+
+	printf("\n------Exercice 1.9 : determiner l'expression binaire d'un nombre N-----\n\n");
+
+	printf("N = %i \n",N);
+
+	printf("L'expression binaire de N est :  ");
+  
+  // Determination de la longueur de l'expression binaire et par suite la longueur du tableau
+  int a = 2;
+  int compteur = 2;
+  while (a < N){
+        a = a*2;
+        compteur++;
+  }
+  
+  // Création du tableau 
+	int binaire[compteur];
+  
+  // La conversion 
+	int n = 0;
+	for (int i = 1; i <= N ; i =i*2) {
+		
+		if ((i & N) == 0) {
+			binaire[n]=0;
+		}
+		else {
+			binaire[n]=1;
+		}
+		n=n+1;
+	}
+	
+	//L'affichage du nombre binaire
+  int i = 0;
+	for (i = n-1 ; i >= 0 ; i--) {
+		printf("%i",binaire[i]);
+	}
+	printf("\n\n");
+
+	return 0 ;
+}
