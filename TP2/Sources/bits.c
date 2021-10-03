019c28b4e50495673db38e9a771ffd27f0565bdc
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-03 17:36:20 +0200

    Update bits.c

--- TP2/Sources/bits.c
+++ TP2/Sources/bits.c
@@ -6,21 +6,22 @@
 
 int main() {
   printf("\n------ Exercice 2.2 : Bits -----\n\n");
+  printf("Si les 4ème et 20ème bits de gauche sont 1 (en binaire) le programme affiche 1 sinon 0\n\n");
 	int d = 87381;
 	int b = 0b00010000000000000001;
-
+	printf("pour d = %d, le programme affiche : ",d);
 	if ( d & b == b){
-	
+
 		printf("%d\n",1);
-		
+
 	}
 
-	else { 
+	else {
 
 		printf("%d\n",0);
 
 	}
 
-
+	printf("\n");
 	return 0 ;
 }
