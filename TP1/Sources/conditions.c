b4f66ce84c60cb717103667ad680122d5df67f46
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-09 11:56:02 +0200

    Update conditions.c

--- TP1/Sources/conditions.c
+++ TP1/Sources/conditions.c
@@ -5,9 +5,10 @@
 /*----------------------------------------------------------------------*/
 
 int main () {
-	printf("\n------ Exercice 1.7 : conditions ------\n\n");
+	printf("\n------ Exercice 1.7 : Affichage des numéros <= 1000 qui satisfaient certaines conditions ------\n\n");
 	
 	/*-------------------------------------------------------------*/
+	// Boucle For pour l'affichage des nombres <= 1000 qui sont divisibles par 2 ou 15
 	printf("les nombres <= 1000 qui sont divisibles par 2 ou 15 sont : \n");
 	for (int i = 0; i <= 1000; i++) {
 		if (i % 2 == 0 & i % 15 == 0) {
@@ -16,6 +17,7 @@ int main () {
 	}
 	
 	/*-------------------------------------------------------------*/
+	// Boucle While .. pour l'affichage des nombres <= 1000 qui sont divisibles par 103 ou 107
 	int j = 0 ;
 	printf("\n\nles nombres <= 1000 qui sont divisibles par 103 ou 107 sont : \n");	
 	while ( j <=1000 ) {
@@ -26,6 +28,7 @@ int main () {
 	}
 	
 	/*-------------------------------------------------------------*/
+	// Boucle Do .... While pour l'affichage des nombres <= 1000 qui sont divisibles par 7 ou 5 mais pas 3
 	int k = 0;
 	printf("\n\nles nombres <= 1000 qui sont divisibles par 7 ou 5 mais pas 3 sont : \n");
 	do {
