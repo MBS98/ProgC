ff26620af5d4e4fc1396eb815687581c6b583d79
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-09 12:13:09 +0200

    Update puissance.c

--- TP2/Sources/puissance.c
+++ TP2/Sources/puissance.c
@@ -6,8 +6,8 @@
 /* Méthode N°1 : Pointeurs*/
 
 int puissance (int *a , int *b){
-int temporaire = *a ;
-for (int i = 1 ; i < *b ; i++) {
+	int temporaire = *a ;
+	for (int i = 1 ; i < *b ; i++) {
 		*a = *a * temporaire;
 	}
 	return *a ;
@@ -15,12 +15,33 @@ for (int i = 1 ; i < *b ; i++) {
 
 /* Main */
 int main () {
-  printf("\n------ Exercice 2.1 : Puissance ------\n\n");
-  
+  printf("\n------ Exercice 2.1 : a puissance b ------\n\n");
+	
+	/* Inialisation des variables */
 	int a = 2;
 	int b = 3;
-	int temporaire = a;
-	int res = puissance(&a,&b);
-	printf("puissance(a = %d , b = %d) est = %d\n",temporaire,b,res);
+	int temporaire = a; // variable temporaire qui va stocker la valeur de a
+	
+	int res = puissance(&a,&b); // appel de la fonction puissance 
+	printf("%d puissance %d = %d\n",temporaire,b,res);
 	return 0 ;
+}
+
+/*---------------------------------------------------------*/
+/* Méthode N°2 : Boucles */
+
+int main (int a, int b) {
+	printf("\n------ Exercice 2.1 : a puissance b ------\n\n");
+	
+	/* Inialisation des variables */
+	a = 3;
+	b = 8;
+	int c = a; // variable temporaire qui va stocker la valeur de a
+	
+	for (int i = 1 ; i < b ; i++) {
+	a = a * c;
+	}
+	printf("%d puissance %d = %d\n", c, b, a);
+
+return 0;
 }
