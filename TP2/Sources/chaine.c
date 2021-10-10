6f1f96a36846973cb9cbdd0a2503f0edb45c0f3b
Silia <siliataider@gmail.com>
2021-10-10 16:01:04 +0200

    Update chaine.c

--- TP2/Sources/chaine.c
+++ TP2/Sources/chaine.c
@@ -6,34 +6,47 @@
 
 int main () {
   printf("\n------ Exercice 2.4 : Chaine ------\n\n");
-	char chaine1[] = "Hello!!";
-	char chaine2[] = "Mariem";
-
-	long int n1 = sizeof(chaine1)/sizeof(char)-1;
-	long int n2 = sizeof(chaine2)/sizeof(char)-1;
-
-	char concat[n1+n2];
-
-	/*La Taille*/
-
-	printf("la taille de la chaine1 %s est %ld\n",chaine1,n1);
-	printf("la taille de la chaine2 %s est %ld\n",chaine2,n2);
-
-	/*Copie*/
 	
-	/*Concaténation*/
-	
-	for (int i = 0; i <= n1; i++) {
-		concat[i] = chaine1[i];
-		concat[n1+i+1] = chaine2[i];
-		
+	char chaine1[] = {"Hello"};
+	char chaine2[] = {"World!"};
+
+	int n1 = (sizeof(chaine1)/sizeof(char))-1;
+	int n2 = (sizeof(chaine2)/sizeof(char))-1;
+	int n3 = n1; //on met la taille de la chaine qu'on souhaite copier
+	int n4 = n1+n2;
+
+	char chaine3[n3]; //copie de chaine 1
+	char chaine4[n4]; //concatenation de la chaine 1 et 2
+
+	printf("La taille de la chaine 1: %d\n", n1);
+	printf("La taille de la chaine 2: %d\n", n2);
+
+	/* copie */
+
+	printf("Chaine3 copie de chaine1:");
+	for (int i = 0; i <= n3; i++) {
+
+	chaine3[i] = chaine1[i];
+	printf("%c", chaine3[i]);
+
 	}
-	printf(" La concaténation des deux chaines donne : ");
 
-	for (int k = 0 ; k <= n1+n2 ; k++){
-	
-	printf("%c",concat[k]);
-	
+	printf("\n");
+
+	/* concatenation */
+
+	for (int j=0; j<=n1; j++){
+
+	chaine4[j]=chaine1[j]; //copie le contenu de chaine1 dans la premiere partie de la chaine
+	chaine4[j+n1+1]=chaine2[j]; // copie le contenu de chaine2 en meme temps dans la 2eme partie
+
+	}
+
+	printf("chaine4 concatenation de chaine 1 et chaine 2: ");
+	for (int k = 0; k <= n4; k++) {
+
+	printf("%c", chaine4[k]);
+
 	}
 	printf("\n");
 	
