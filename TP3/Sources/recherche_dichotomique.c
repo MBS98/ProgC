db125072e99dbd6270afa6bd126236ee64b26550
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-10 17:20:31 +0200

    Update recherche_dichotomique.c

--- TP3/Sources/recherche_dichotomique.c
+++ TP3/Sources/recherche_dichotomique.c
@@ -1,11 +1,14 @@
+/* Bibliothèques */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+/*---------------------------------------------------------*/
+
 int main(){
 
-      srand(time(0));  //pour eviter qu on genere la meme sequence de nombres aleatoires a chaque compilation
-
+      srand(time(0));  //pour eviter qu'on genere la meme sequence de nombres aleatoires a chaque compilation
       int n = 10;
       int tab[n];
       int tab_tri[n];
@@ -57,6 +60,79 @@ int main(){
         }
         if (debut > fin)
           printf("L'entier %d n'est pas present dans la liste.\n", recherche);
+      
+      /* Methode N°2 
+      // Déclaration des variables
+	int longueur = 10;
+	int tab [longueur];
+	int *p = NULL;
+	int compt = 0;
+	int val = 28; // Variable à trouver
+	int debut = 0;
+	int fin = longueur;
+	int milieu = (fin+debut)/2;
 
+      // Création du tableau
+	//srand(time(NULL));
+	for (p = tab ; p < tab + longueur; p++) {
+		
+		*p = rand() % 101; 
+	}
+	printf("\n");
+
+      // Affichage du tableau
+	printf("le tableau non trié: \n");
+	for ( int j = 0 ; j < longueur ; j ++ ) {
+		printf("%d ", tab[j]);
+	}
+	printf("\n");
+      // Tri du tableau
+	for (int i = 0 ; i < longueur ; i ++) {
+		for ( int l = 0 ; l < i ; l ++) {
+			if ( tab [i] < tab [l]){
+			inverse(&tab[i],&tab[l]);
+			}
+		}
+	}
+
+      // Affichage du tableau trié
+	printf("le tableau trié : \n");
+	for ( int k = 0 ; k < longueur ; k ++ ) {
+		printf("%d ", tab[k]);
+	}
+	printf("\n");
+
+      // Recherche dichotomique 
+
+	while ((debut <= fin)) {
+		if ( tab[milieu] > val ){
+			fin = milieu - 1;
+			compt = 0 ;
+			
+		}
+		else if ( tab[milieu] < val ) {
+			debut = milieu + 1;
+			compt = 0;
+		}
+		else if ((debut == milieu) && (tab[debut]!= val)) {
+			compt = 0;
+			break;
+		}
+		else {
+			compt = 1;
+			break;
+		}
+	milieu = (fin+debut)/2;
+	}
+	
+	if (compt == 1) {
+		printf("l'entier %d est présent dans le tableau",val);
+	}
+	else {
+		printf("l'entier %d n'est pas présent dans le tableau",val);
+	}	
+	printf("\n");
+      
+      */ 
       return 0;
 }
