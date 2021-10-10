c3f4cc8a4254a21436149aac3e42919b0484d77b
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-10 17:04:23 +0200

    Update tri.c

--- TP3/Sources/tri.c
+++ TP3/Sources/tri.c
@@ -1,6 +1,10 @@
+/* Bibliothèques */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+/*---------------------------------------------------------*/
 
 int main(){
 
@@ -9,7 +13,7 @@ int main(){
       int tab_tri[n];
       int c;
 
-      srand(time(0));  //pour eviter qu on genere la meme sequence de nombres aleatoires a chaque compilation
+      srand(time(0));  //pour eviter qu'on genere la meme sequence de nombres aleatoires a chaque compilation
 
       for (int i=0; i<n; i++){ //on initialise les valeurs du tableau avec des valeurs aleatoires 
       tab[i]=rand();
@@ -17,7 +21,7 @@ int main(){
 
       printf("\nTableau non trie:\n");
 
-      for (int j=0; j<n; j++){ //affichage du tableau non trie 
+      for (int j=0; j<n; j++){ //affichage du tableau non trié 
       printf("%i\n",tab[j]);
       }
 
@@ -35,9 +39,47 @@ int main(){
       }
       printf("\nTableau trie:\n");
 
-      for (int j=0; j<n; j++){ //affichage du tableau trie
+      for (int j=0; j<n; j++){ //affichage du tableau trié
       printf("%i\n",tab[j]);
       }
+      
+      /* Methode N°2 
+      int tab [10];
+	int *p = NULL;
+
+      // Création du tableau
+	srand(time(NULL));
+	for (p = tab ; p < tab + 10; p++) {
+		
+		*p = rand() % 101; 
+	}
+	
+
+	printf("\n");
+
+      // Affichage du tableau
+	for ( int j = 0 ; j < 10 ; j ++ ) {
+		printf("%d ", tab[j]);
+	}
+	printf("\n");
+
+      // Tri du tableau
+	for (int i = 0 ; i < 10 ; i ++) {
+		for ( int l = 0 ; l < i ; l ++) {
+				if ( tab [i] < tab [l]){
+				inverse(&tab[i],&tab[l]);
+				}
+		}
+
+	}
+      // Affichage du tableau trié
+	for ( int k = 0 ; k < 10 ; k ++ ) {
+		printf("%d ", tab[k]);
+	}
+	printf("\n");
+      
+      */
+      
       return 0;
 
 }
