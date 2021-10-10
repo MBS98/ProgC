fd839d3909f65c19bb28eae4bd6ef7b16ed0055d
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-10 14:34:13 +0200

    Update fibonacci.c

--- TP2/Sources/fibonacci.c
+++ TP2/Sources/fibonacci.c
@@ -8,10 +8,20 @@
 #define n 11
 
 int main() {
-	printf("\n------ Exercice 2.3 : Fibonacci ------\n\n");
+	printf("\n------ Exercice 2.3 : La suite de Fibonacci ------\n\n");
 	int U0 = 1;
 	int U1 = 1;
 	int tab[n] = {U0,U1};
+	
+	/* Autre méthode pour la déclaration de la taille du tableau et du tableau lui même :
+	int n = 10;
+	int u0 = 1;
+	int u1 = 1;
+	int U[n];
+	int taille = n+1;
+	U[0]=u0;
+	U[1]=u1; */
+	
 	for (int i = 2 ; i <= n ; i++) {
 	
 		tab[i] = tab[i-1] + tab[i-2];
