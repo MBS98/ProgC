b3d7d4fe65caa7bbfe0d6f9dc2d778820f532dfa
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-10 16:51:22 +0200

    Update tableauptr.c

--- TP2/Sources/tableauptr.c
+++ TP2/Sources/tableauptr.c
@@ -10,26 +10,26 @@ int main () {
 
 int n = 10; //indice du tableau int
 int f = 11; //indice du tableau float
-int c = 0; //variable qui s incrementera avec les indices du tableau
+int c = 0; //variable qui s'incrementera avec les indices du tableau
 
 int int_tab [n]; //on cree un tableau de int
 float tab [f]; //on cree un tableau de float
 
-float *p = NULL; //on initialise un pointure qui pointe vers NULL pour ne pas prendre le risque de pointer vers un espace aléatoire de la mémoire
+float *p = NULL; //on initialise un pointeur qui pointe vers NULL pour ne pas prendre le risque de pointer vers un espace aléatoire de la mémoire
 
 printf("Initialisation du tableau de int : \n");
 
 for (p=tab; p<tab + f; p++){
 
-*p = rand(); //on parcourt le tableau et remplit ses alement avec des nombres aleatoires entre 0 et RAND_MAX
+*p = rand(); //on parcourt le tableau et remplit ses elements avec des nombres aleatoires entre 0 et RAND_MAX
 
-if (c%2==0){ //si l indice du de la case est paire on multiplie son contenu par 3
+if (c%2==0){ //si l'indice de la case est paire on multiplie son contenu par 3
 
 *p = *p * 3;
 
 }
 
-c++; //on incremente de 1 la variable d indice
+c++; //on incremente de 1 la variable d'indice
 
 }
 
@@ -37,13 +37,13 @@ c = 0; //on remet c a 0
 
 for (p=tab; p<tab + f; p++){ //on affiche les valeurs du tableau 
 
-if (c%2==0){ //si l indice du de la case est paire on affiche la phrase qui suit
+if (c%2==0){ //si l'indice de la case est paire on affiche la phrase qui suit
 
-printf("Case %i (valeur x3): %f\n", c, *p);
+printf("Case %i (valeur x3): %d\n", c, *p);
 }
 
 else{
-printf("Case %d: %f\n", c, *p);
+printf("Case %d: %d\n", c, *p);
 }
 
 c++; //on incremente c de 1
@@ -68,7 +68,7 @@ c++;
 
 for (p=tab; p<tab + f; p++){ //on affiche les valeurs du tableau 
 
-if (c%2==0){ //si l indice du de la case est paire on affiche la phrase qui suit
+if (c%2==0){ //si l'indice de la case est paire on affiche la phrase qui suit
 
 printf("Case %i (valeur x3): %f\n", c, *p);
 }
