8f5dbec1678b4e40b021be71d49a9c06225b86aa
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-10 17:02:23 +0200

    Update grand_petit.c

--- TP3/Sources/grand_petit.c
+++ TP3/Sources/grand_petit.c
@@ -1,34 +1,39 @@
+/* Bibliothèques */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+/*---------------------------------------------------------*/
+
 int main(){
 
-      int n=100; //initialisation des variables
+      // Initialisation des variables
+      int n = 100; 
       int tab[n];
-      int petit=0;
-      int grand=0;
+      int petit = 0;
+      int grand = 0;
 
-      srand(time(0)); //pour eviter qu on genere la meme sequence de nombres aleatoires a chaque compilation
+      srand(time(0)); //pour eviter qu'on genere la meme sequence de nombres aleatoires a chaque compilation
 
-      for (int i=0; i<n; i++){ //initialisation du tableau avec des valeurs random
-      tab[i]=rand();
+      for (int i = 0 ; i < n ; i++){ //initialisation du tableau avec des valeurs random
+      tab[i] = rand();
       }
 
-      for (int j=0; j<n; j++){ //affichage du tableau
+      for (int j = 0 ; j < n ; j++){ //affichage du tableau
       printf("%i\n",tab[j]);
       }
 
       petit = tab[0];
 
-      for (int i=0; i<n; i++){ //on parcourt le tableau et on compare la valeur courante
+      for (int i = 0; i < n ; i++){ //on parcourt le tableau et on compare la valeur courante
 
-      if (tab[i]<petit){ //si elle est inferieure a la valeur stoquee dans petit on permute
+      if (tab[i] < petit){ //si elle est inferieure a la valeur stoquee dans petit on permute
       petit = tab[i];
       }
 
 
-      if (tab[i]>grand){ //si elle est superieure a la valeur stoquee dans grand on permute
+      if (tab[i] > grand){ //si elle est superieure a la valeur stoquee dans grand on permute
       grand = tab[i];
       }
 
@@ -36,6 +41,38 @@ int main(){
 
       printf("\nLa valeur la plus petite: %i\n", petit);
       printf("La valeur la plus grande: %i\n", grand);
+      
+      /* Méthode N°2 
+      srand(time(0));
+	int tab [100];
+	int *p = NULL;
+	for (p = tab ; p < tab + 100 ; p++) {
+		*p = rand() ; 
+	}
+	
+	int max = tab[0];
+	int min = tab[0];
+	printf("\n");
+	for ( int j = 0 ; j < 100 ; j ++ ) {
+		printf("%d ", tab[j]);
+	}
+	printf("\n");
+	for ( int i = 0 ; i < 100 ; i ++) {
+		if ( tab [i] > max ) {
+		max = tab[i];
+		}
+		else if ( tab[i] < min ) {
+		min = tab[i];
+		}
+
+	}
+	printf("\n");
+	printf("la plus grande valeur dans ce tableau est : %d\n",max);
+	printf("la plus petite valeur dans ce tableau est : %d\n",min);
+	printf("\n");
+      
+      */
+      
       
       return 0;
 }
