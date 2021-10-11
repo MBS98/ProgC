86eb8baa70e99f45c162410800b964a9184bf7ed
Silia <siliataider@gmail.com>
2021-10-11 23:28:52 +0200

    Update chercher2.c

--- TP3/Sources/chercher2.c
+++ TP3/Sources/chercher2.c
@@ -1,18 +1,24 @@
-// on cree un tableau de tableaux qu on appelle tab et qui contient 10 phrases
-// on declare str1 une phrase qu on precisera et dont on va verifier l existance dans tab
-// on declare 2 variables oui = 0 et non = 0
-// on rentre dans une boucle for qui va parcourir les n lignes de tab
-// pour chaque ligne, on copie le contenu de tab (de la ligne courante) dans str2
-// on rentre dans une autre boucle for qui va comparer les contenu des 2 phrases (str1 et str2) case par case
-// si un element de str2 no correspond pas a lelement str2 de la meme case, on "break" et on sort de la boucle
-// on met oui a 0 si les elements ne se correspondent pas
-// p.s. la comparaison se fait en comparant les codes ASCII
-// si les elements se correspondent on met la variable oui a 1 et on continue
-// si le prochain element ne correspond pas on remet oui a 0, sinon on laisse a 1
-// si on arrive a la fin de la phrase et que oui est a 1 on print que la phrase est presente
-// et on met la variable non a 1
-// sinon on passe a la phrase d apres (la ligne suivante de tab) et on garde la variable non a 0
-// si on arrive a la fin du tableau et que non est toujours a 0 on print que la phrase n est pas presente
+/* Fichier: octets.c
+
+ *on cree un tableau de tableaux qu on appelle tab et qui contient 10 phrases
+ on declare str1 une phrase qu on precisera et dont on va verifier l existance dans tab
+ on declare 2 variables oui = 0 et non = 0
+ on rentre dans une boucle for qui va parcourir les n lignes de tab
+ pour chaque ligne, on copie le contenu de tab (de la ligne courante) dans str2
+ on rentre dans une autre boucle for qui va comparer les contenu des 2 phrases (str1 et str2) case par case
+ si un element de str2 no correspond pas a lelement str2 de la meme case, on "break" et on sort de la boucle
+ on met oui a 0 si les elements ne se correspondent pas
+ p.s. la comparaison se fait en comparant les codes ASCII
+ si les elements se correspondent on met la variable oui a 1 et on continue
+ si le prochain element ne correspond pas on remet oui a 0, sinon on laisse a 1
+ si on arrive a la fin de la phrase et que oui est a 1 on print que la phrase est presente
+ et on met la variable non a 1
+ sinon on passe a la phrase d apres (la ligne suivante de tab) et on garde la variable non a 0
+ si on arrive a la fin du tableau et que non est toujours a 0 on print que la phrase n est pas presente
+ 
+ * auteur: Silia TAIDER - Mariem Ben Salah - Omar Gasri
+
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
