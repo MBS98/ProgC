03877fcdb1fb3779604ca01d118756d12ca1e101
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-11 14:09:11 +0200

    Update octets.c

--- TP3/Sources/octets.c
+++ TP3/Sources/octets.c
@@ -10,16 +10,13 @@
   // Taille d'une variable de type short
   short s = 100; //on definit la variable
   char *ptrs = (char*)&s; //on definit un pointeur
-  printf("la taille de la variable de type short est = ");
   for (i = 0 ; i < sizeof(s) ; i++){ //on parcourt tous les octets de la variable
       ptrs++; //on incremente le pointeur pour passer a l'octet suivant
       printf("%hhx",ptrs);
-
   }
   printf("\n");
 
   // Taille d'une variable de type int
-  printf("la taille de la variable de type int est = ");
   int d = 12365;
   char *ptrd = (char*)&d;
   for (i = 0 ; i < sizeof(d) ; i++){
@@ -29,7 +26,6 @@
   printf("\n");
 
   // Taille d'une variable de type long int
-  printf("la taille de la variable de type long int est = ");
   long int ld = 15425;
   char *ptrld = (char*)&ld;
   for (i = 0 ; i < sizeof(ld) ; i++){
@@ -39,7 +35,6 @@
   printf("\n");
 
   // Taille d'une variable de type float
-  printf("la taille de la variable de type float est = ");
   float f = 13.8965;
   char *ptrf = (char*)&f;
   for (i = 0 ; i < sizeof(f) ; i++){
@@ -49,7 +44,6 @@
   printf("\n");
 
   // Taille d'une variable de type double
-  printf("la taille de la variable de type double est = ");
   double g = 4.569;
   char *ptrg = (char*)&g;
   for (i = 0 ; i < sizeof(g) ; i++){
