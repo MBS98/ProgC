bc2a3a289a2648d061f9d10c90d59278c55dc262
Silia <siliataider@gmail.com>
2021-10-11 22:31:27 +0200

    Update tableauptr.c

--- TP2/Sources/tableauptr.c
+++ TP2/Sources/tableauptr.c
@@ -8,79 +8,85 @@
 
 int main () {
 
-int n = 10; //indice du tableau int
-int f = 11; //indice du tableau float
-int c = 0; //variable qui s'incrementera avec les indices du tableau
+	int n = 10; //indice du tableau int
+	int f = 11; //indice du tableau float
+	int c = 0; //variable qui s'incrementera avec les indices du tableau
 
-int int_tab [n]; //on cree un tableau de int
-float tab [f]; //on cree un tableau de float
+	int int_tab [n]; //on cree un tableau de int
+	float tab [f]; //on cree un tableau de float
 
-float *p = NULL; //on initialise un pointeur qui pointe vers NULL pour ne pas prendre le risque de pointer vers un espace aléatoire de la mémoire
+	float *p = NULL; //on initialise un pointeur qui pointe vers NULL pour ne pas prendre le risque de pointer vers un espace aléatoire de la mémoire
 
-printf("Initialisation du tableau de int : \n");
+	printf("Initialisation du tableau de int : \n");
 
-for (p=tab; p<tab + f; p++){
+	/* METHODE: initialisaiton avec rand  */
+	  
 
-*p = rand(); //on parcourt le tableau et remplit ses elements avec des nombres aleatoires entre 0 et RAND_MAX
+	for (p=int_tab; p<int_tab + n; p++){
 
-if (c%2==0){ //si l'indice de la case est paire on multiplie son contenu par 3
+		*p = rand(); //on parcourt le tableau et remplit ses elements avec des entiers aleatoires entre 0 et RAND_MAX
 
-*p = *p * 3;
+		if (c%2==0){ //si l'indice de la case est paire on multiplie son contenu par 3
 
+			*p = *p * 3;
+
+		}
+
+		c++; //on incremente de 1 la variable d'indice
+
+	}
+	  
+	  
+	c = 0; //on remet c a 0 
+
+	for (p=int_tab; p<int_tab + n; p++){ //on affiche les valeurs du tableau 
+
+		if (c%2==0){ //si l'indice de la case est paire on affiche la phrase qui suit
+
+		printf("Case %i (valeur x3): %f\n", c, *p);
+		}
+
+		else{
+		printf("Case %d: %f\n", c, *p);
+		}
+
+		c++; //on incremente c de 1
+	}
+
+	//////////////////////////////////////
+
+	printf("\nInitialisation du tableau de float : \n"); //on refait de meme pour le tableau des float
+	c = 0; //on remet c a 0 
+
+	for (p=tab; p<tab + f; p++){
+
+		*p = (float)rand()/RAND_MAX; 
+
+		if (c%2==0){
+
+			*p = *p * 3;
+		}
+
+		c++;
+
+	}
+
+	c = 0; //on remet c a 0
+
+	for (p=tab; p<tab + f; p++){ //on affiche les valeurs du tableau 
+
+		if (c%2==0){ //si l'indice de la case est paire on affiche la phrase qui suit
+
+			printf("Case %i (valeur x3): %f\n", c, *p);
+		}
+
+		else{
+			printf("Case %d: %f\n", c, *p);
+		}
+
+		c++; //on incremente c de 1
+	}
+
+
+	return 0;
 }
-
-c++; //on incremente de 1 la variable d'indice
-
-}
-
-c = 0; //on remet c a 0 
-
-for (p=tab; p<tab + f; p++){ //on affiche les valeurs du tableau 
-
-if (c%2==0){ //si l'indice de la case est paire on affiche la phrase qui suit
-
-printf("Case %i (valeur x3): %f\n", c, *p);
-}
-
-else{
-printf("Case %d: %f\n", c, *p);
-}
-
-c++; //on incremente c de 1
-}
-
-//////////////////////////////////////
-
-printf("\nInitialisation du tableau de float : \n"); //on refait de meme pour le tableau des float
-
-for (p=tab; p<tab + f; p++){
-
-*p = rand(); 
-
-if (c%2==0){
-
-*p = *p * 3;
-}
-
-c++;
-
-}
-
-for (p=tab; p<tab + f; p++){ //on affiche les valeurs du tableau 
-
-if (c%2==0){ //si l'indice de la case est paire on affiche la phrase qui suit
-
-printf("Case %i (valeur x3): %f\n", c, *p);
-}
-
-else{
-printf("Case %d: %f\n", c, *p);
-}
-
-c++; //on incremente c de 1
-}
-
-
-return 0;
-}
-
