410b2082a2d7f03e0068922c926409e3c976be8a
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-10-24 21:35:06 +0200

    Add files via upload

---
+++ TP4/src/operator.c
@@ -0,0 +1,47 @@
+// Fonction somme
+
+int add (int num1 , int num2 ) {
+	return num1 + num2 ;
+}
+
+// Fonction différence 
+
+int sub (int num1 , int num2 ) {
+	return num1 - num2 ;
+}
+
+// Fonction produit 
+
+int multp (int num1 , int num2 ) {
+	return num1 * num2 ;
+}
+
+// Fonction quotient 
+
+int quot (int num1 , int num2 ) {
+	return num1 / num2 ;
+}
+
+// Fonction modulo 
+
+int modulo (int num1 , int num2 ) {
+	return num1 % num2 ;
+}
+
+// Fonction et 
+
+int et (int num1 , int num2 ) {
+	return num1 & num2 ;
+}
+
+// Fonction ou 
+
+int ou (int num1 , int num2 ) {
+	return num1 | num2 ;
+}
+
+// Fonction négation 
+
+int non (int num1 ) {
+	return !num1 ;
+}
