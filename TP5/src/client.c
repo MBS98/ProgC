0b2b44e9524f247930f372bd6e6389220ba1ab88
Silia <siliataider@gmail.com>
2021-11-07 15:10:36 +0100

    Add files via upload

---
+++ TP5/src/client.c
@@ -0,0 +1,145 @@
+/*
+ * SPDX-FileCopyrightText: 2021 John Samuel
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "client.h"
+
+/* 
+ * Fonction d'envoi et de réception de messages
+ * Il faut un argument : l'identifiant de la socket
+ */
+
+int envoie_recois_message(int socketfd){
+
+  char data[1024];
+  memset(data, 0, sizeof(data));
+
+  char message[100];
+  /*char op[10];
+  char num1[10];
+  char num2[10];*/
+
+  printf("Rentrer sous la forme: operateur num1 num2 \n");
+  fgets(message, 1024, stdin);
+
+  /*printf("Rentrer le num 1 ");
+  fgets(num1, 1024, stdin);
+
+  printf("Rentrer le num 2 ");
+  fgets(num2, 1024, stdin);*/
+
+  strcpy(data, "calcule: ");
+  strcat(data, message);
+  /*strcpy(data, op);
+  strcat(data, " ");
+  strcat(data, num1);
+  strcat(data, " ");
+  strcat(data, num2);*/
+  
+  //printf("data = %s\n", data);
+
+  int write_status = write(socketfd, data, strlen(data));
+  if ( write_status < 0 ) {
+    perror("erreur ecriture");
+    exit(EXIT_FAILURE);
+  }
+
+  // la réinitialisation de l'ensemble des données
+  memset(data, 0, sizeof(data));
+
+
+  // lire les données de la socket
+  int read_status = read(socketfd, data, sizeof(data));
+  if ( read_status < 0 ) {
+    perror("erreur lecture");
+    return -1;
+  }
+
+  printf("Message client recu: %s\n", data);
+ 
+  return 0;
+}
+
+  
+/*int envoie_recois_message(int socketfd) {
+ 
+  char data[1024];
+  // la réinitialisation de l'ensemble des données
+  memset(data, 0, sizeof(data));
+
+
+  // Demandez à l'utilisateur d'entrer un message
+  char message[100];
+  printf("Votre message (max 1000 caracteres): ");
+  fgets(message, 1024, stdin);
+  strcpy(data, "message: ");
+  strcat(data, message);
+  
+  int write_status = write(socketfd, data, strlen(data));
+  if ( write_status < 0 ) {
+    perror("erreur ecriture");
+    exit(EXIT_FAILURE);
+  }
+
+  // la réinitialisation de l'ensemble des données
+  memset(data, 0, sizeof(data));
+
+
+  // lire les données de la socket
+  int read_status = read(socketfd, data, sizeof(data));
+  if ( read_status < 0 ) {
+    perror("erreur lecture");
+    return -1;
+  }
+
+  printf("Message recu: %s\n", data);
+ 
+  return 0;
+}*/
+
+int main() {
+  int socketfd;
+  int bind_status;
+
+  struct sockaddr_in server_addr, client_addr;
+
+  /*
+   * Creation d'une socket
+   */
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  if ( socketfd < 0 ) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  //détails du serveur (adresse et port)
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+
+  //demande de connection au serveur
+  int connect_status = connect(socketfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+  if ( connect_status < 0 ) {
+    perror("connection serveur");
+    exit(EXIT_FAILURE);
+  }
+
+  // appeler la fonction pour envoyer un message au serveur
+  envoie_recois_message(socketfd);
+  //envoie_operateur_numeros(socketfd);
+
+  close(socketfd);
+
+}
