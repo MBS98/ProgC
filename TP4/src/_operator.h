af23901c192a69d88320bb5317df83b648da5910
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-11-08 23:27:10 +0100

    Delete _operator.h

--- TP4/src/_operator.h
+++
@@ -1,8 +0,0 @@
-int add ( int , int );
-int sub ( int , int );
-int multp ( int , int );
-int quot ( int , int );
-int modulo ( int , int );
-int et ( int , int );
-int ou ( int , int );
-int non ( int );
