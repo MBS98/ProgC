dbe52867e143a885cd3add1b2605fa6647e6ab7d
Mariem Ben Salah <72150651+Mariem-Ben-Salah@users.noreply.github.com>
2021-11-08 23:27:02 +0100

    Create operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,8 @@
+int add ( int , int );
+int sub ( int , int );
+int multp ( int , int );
+int quot ( int , int );
+int modulo ( int , int );
+int et ( int , int );
+int ou ( int , int );
+int non ( int );
